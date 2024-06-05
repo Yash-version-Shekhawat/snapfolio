82e59d66bdb22bb6520e2c0806825227b9ebd7ea
yashvardhan <yashuitjy112@gmail.com>
2024-06-05 16:15:41 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ function App() {
               </button>
             </div>
             <div><img className=' w-[29vw] md:w-[15vw] md:-mt-[0.5vw]' src={jour}/></div>
-            <div className='main flex items-center justify-center w-[100%] h-[60vw] md:h-[50%] mb-[10vw] md:mt-[2.5vw] over'>
+            <div className='main flex items-center justify-center w-[100%] h-[60vw] md:h-[50%] mb-[10vw] md:mt-[2.5vw] overflow-hidden'>
               <div className='pacman relative bg-transparent z-[1] ml-[2vw]'>
                 <div className='h-[0.8rem] w-[0.8rem] bg-[#1E1E1E] absolute z-[999] rounded-[2rem] -mt-[2rem] ml-[3.5rem] '></div>
               </div>
